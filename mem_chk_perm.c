5320d0a41784ad620a8de5147e662897c70a9dc8
Alessandro Carminati <alessandro.carminati@gmail.com>
2025-04-03 23:23:57 +0200

    Monothread is working as expected
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- mem_chk_perm.c
+++ mem_chk_perm.c
@@ -7,8 +7,10 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <setjmp.h>
 
 #define MAX_REGIONS 256
+//#define DEBUG
 
 #if __SIZEOF_POINTER__ == 8
 #define ADDRESS_SPACE_END 0xFFFFFFFFFFFFFFFF
@@ -21,11 +23,10 @@
 #define ANSWER_TO_THE_ULTIMATE_QUESTION 42
 
 #define LINE_FORMAT_REGULAR  "| 0x%016lx - 0x%016lx | %s | %-40s | %-29s | %-29s | %-29s |\n"
-#define LINE_FORMAT_MODIFIED "|*0x%016lx - 0x%016lx*| %s | %-40s | %-29s | %-29s | %-29s |\n"
+#define LINE_FORMAT_MODIFIED "| \033[33m0x%016lx - 0x%016lx\033[0m | %s | %-40s | %-29s | %-29s | %-29s |\n"
 
 
 typedef struct {
-	pid_t pid[3];
 	int status[3];
 	unsigned long start;
 	unsigned long end;
@@ -39,7 +40,45 @@ typedef struct {
 } MemMap;
 
 
-const char *reasons[3]={ "\033[33muntested\033[0m", "\033[32mwritten ok\033[0m", "\033[31msegfault\033[0m" };
+const char *reasons[3]={ "\033[33muntested\033[0m", "\033[32mwritten ok\033[0m", "\033[31mdenied\033[0m" };
+
+static sigjmp_buf jump_buffer;
+
+void signal_handler(int signum) {
+	(void)signum;
+	siglongjmp(jump_buffer, 1);
+}
+
+int safe_write(void *addr, int value) {
+	struct sigaction sa, old_sa_segv, old_sa_bus;
+	int old;
+
+	sa.sa_handler = signal_handler;
+	sigemptyset(&sa.sa_mask);
+	sa.sa_flags = 0;
+	sigaction(SIGSEGV, &sa, &old_sa_segv);
+	sigaction(SIGBUS, &sa, &old_sa_bus);
+
+	if (sigsetjmp(jump_buffer, 1) == 0) {
+		volatile int *volatile_addr = (volatile int *)addr;
+		old = *volatile_addr;
+		*volatile_addr = value;
+		if (*volatile_addr != value) {
+			printf("weird happened");
+			exit(2);
+		}
+		*volatile_addr = old;
+
+		sigaction(SIGSEGV, &old_sa_segv, NULL);
+		sigaction(SIGBUS, &old_sa_bus, NULL);
+		return WRITEOK;
+	}
+
+	sigaction(SIGSEGV, &old_sa_segv, NULL);
+	sigaction(SIGBUS, &old_sa_bus, NULL);
+	return WRITEKO;
+}
+
 
 void parse_proc_maps(MemMap *m) {
 	FILE *fp = fopen("/proc/self/maps", "r");
@@ -83,18 +122,16 @@ void parse_proc_maps(MemMap *m) {
 			prev_end = region->end;
 		}
 	memset(m->regions[m->region_count-1].status, 0, sizeof(m->regions[m->region_count-1].status));
-	memset(m->regions[m->region_count-1].pid, 0, sizeof(m->regions[m->region_count-1].pid));
-	printf("%lx-%lx %s %s [%d,%d,%d] [%d,%d,%d]\n", 
+#ifdef DEBUG
+	printf("%lx-%lx %s %s [%d,%d,%d]\n", 
 			m->regions[m->region_count-1].start, 
 			m->regions[m->region_count-1].end, 
 			m->regions[m->region_count-1].perms, 
 			m->regions[m->region_count-1].name, 
-			m->regions[m->region_count-1].pid[0], 
-			m->regions[m->region_count-1].pid[1], 
-			m->regions[m->region_count-1].pid[2], 
 			m->regions[m->region_count-1].status[0], 
 			m->regions[m->region_count-1].status[1], 
 			m->regions[m->region_count-1].status[2]);
+#endif
 
 	}
 	fclose(fp);
@@ -116,92 +153,27 @@ void print_memory_map(MemMap *m, MemMap *m2) {
 
 		printf("+-----------------------------------------+------+------------------------------------------+----------------------+----------------------+----------------------+\n");
 		printf( ((m->regions[i].start == m2->regions[i].start) && (m->regions[i].end == m2->regions[i].end)) ? LINE_FORMAT_REGULAR : LINE_FORMAT_MODIFIED,
-//		printf( LINE_FORMAT_REGULAR,
 			   m->regions[i].start, m->regions[i].end, m->regions[i].perms, m->regions[i].name[0] ? m->regions[i].name : "[anonymous]",
 			   reasons[m->regions[i].status[0]], reasons[m->regions[i].status[1]], reasons[m->regions[i].status[2]] );
-//			   reasons[0], reasons[0], reasons[0] );
 	}
 	printf("+-----------------------------------------+------+------------------------------------------+----------------------+----------------------+----------------------+\n");
 }
 
-void handle_child_exit(MemMap *m, pid_t pid, int status) {
-	int i,j;
-
-	for (j = 0; j < 3; j++) {
-		for (i = 0; i < m->region_count; i++) {
-			if (m->regions[i].pid[j] == pid) {
-				if (WIFEXITED(status)) {
-					m->regions[i].status[j] = WRITEOK;
-				}
-				if (WIFSIGNALED(status)) {
-					m->regions[i].status[j] = WRITEKO;
-				}
-				return;
-			}
-		}
-	}
-	printf("[handle_child_exit]unknown pid (%d) exit\n", pid);
-}
-
-void child_process(MemRegion *region, int test_type) {
-	pid_t pid = getpid();
-	int *ptr;
-	int old;
-	int i;
-
-	prctl(PR_SET_DUMPABLE, 0);
-
-	unsigned long addresses[3] = {region->start, (region->start + region->end) / 2, region->end - 2 * sizeof(int)};
-	ptr = (int *)addresses[test_type];
-	printf("[%d] read %p region=%s test_type=%d\n", pid, ptr, region->name, test_type);
-	old = *ptr; // save old
-	printf("[%d] read %p -> %d\n", pid, ptr, old);
-
-	*ptr = ANSWER_TO_THE_ULTIMATE_QUESTION;
-	printf("[%d] read %p <- %d\n", pid, ptr, ANSWER_TO_THE_ULTIMATE_QUESTION);
-
-	if (*ptr != ANSWER_TO_THE_ULTIMATE_QUESTION) {
-		printf("the write at %p was not successful (%d)\n", ptr, *ptr);
-		exit(2);
-	}
-	printf("[%d] read %p <- %d\n", pid, ptr, old);
-	*ptr = old;
-	exit(0);
-}
-
 int main(int argc, char *argv[]) {
-	void *rsp;
-
 	MemMap *m, *m2;
 
 	m = malloc(sizeof(MemMap));
 	m2 = malloc(sizeof(MemMap));
-	getchar();
 	memset(m, 0, sizeof(m));
 	memset(m2, 0, sizeof(m2));
-	__asm__("mov %%rsp, %0" : "=r"(rsp));printf("Stack Pointer (RSP): %p\n", rsp);
  	parse_proc_maps(m);
-	getchar();
-//        print_memory_map(&m, &m2);
-	for (int j = 0; j<3; j++){
-		for (int i = 0; i < m->region_count; i++) {
-			pid_t pid = fork();
-			if (pid < 0) {
-				perror("Fork failed");
-				exit(EXIT_FAILURE);
-			} else if (pid == 0) {
-				child_process(&(m->regions[i]), j);
-			} else {
-				m->regions[i].pid[j] = pid;
-			}
-		}
-	}
-
-	for (int j = 0; j<3; j++){
-		for (int i = 0; i < m->region_count; i++) {
-			int status;
-			pid_t pid = wait(&status);
-			handle_child_exit(m, pid, status);
+	for (int i = 0; i < m->region_count; i++) {
+		unsigned long addresses[3] = {m->regions[i].start, (m->regions[i].start + m->regions[i].end) / 2, m->regions[i].end - 2 * sizeof(int)};
+		for (int j = 0; j<3; j++){
+#ifdef DEBUG
+			printf("Attempt write %s at %p\n", m->regions[i].name, (int *) addresses[j]);
+#endif
+			m->regions[i].status[j] = safe_write((int *) addresses[j], ANSWER_TO_THE_ULTIMATE_QUESTION);
 		}
 	}
 
@@ -210,7 +182,6 @@ int main(int argc, char *argv[]) {
 	free(m);
 	free(m2);
 
-	sleep(300);
 	return 0;
 }
 
