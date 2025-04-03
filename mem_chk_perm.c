19ab0e16cd17396676edd4577b67aa4fc3dc4c26
Alessandro Carminati <alessandro.carminati@gmail.com>
2025-04-03 22:34:22 +0200

    last attempt multithread. it can't work
    Signed-off-by: Alessandro Carminati <alessandro.carminati@gmail.com>

--- mem_chk_perm.c
+++ mem_chk_perm.c
@@ -8,7 +8,7 @@
 #include <sys/wait.h>
 #include <signal.h>
 
-#define MAX_REGIONS 1024
+#define MAX_REGIONS 256
 
 #if __SIZEOF_POINTER__ == 8
 #define ADDRESS_SPACE_END 0xFFFFFFFFFFFFFFFF
@@ -20,6 +20,9 @@
 #define WRITEKO 2
 #define ANSWER_TO_THE_ULTIMATE_QUESTION 42
 
+#define LINE_FORMAT_REGULAR  "| 0x%016lx - 0x%016lx | %s | %-40s | %-29s | %-29s | %-29s |\n"
+#define LINE_FORMAT_MODIFIED "|*0x%016lx - 0x%016lx*| %s | %-40s | %-29s | %-29s | %-29s |\n"
+
 
 typedef struct {
 	pid_t pid[3];
@@ -30,12 +33,18 @@ typedef struct {
 	char name[256];
 } MemRegion;
 
-const char *reasons[3]={ "\033[33muntested\033[0m", "\033[32mwritten ok\033[0m", "\033[31msegfault\033[0m" };
-MemRegion regions[MAX_REGIONS];
-int region_count = 0;
+typedef struct {
+	MemRegion regions[MAX_REGIONS];
+	int region_count;
+} MemMap;
 
-void parse_proc_maps() {
+
+const char *reasons[3]={ "\033[33muntested\033[0m", "\033[32mwritten ok\033[0m", "\033[31msegfault\033[0m" };
+
+void parse_proc_maps(MemMap *m) {
 	FILE *fp = fopen("/proc/self/maps", "r");
+	MemRegion *region = malloc(sizeof(MemRegion));
+
 	if (!fp) {
 		perror("Failed to open /proc/self/maps");
 		exit(EXIT_FAILURE);
@@ -44,72 +53,88 @@ void parse_proc_maps() {
 	unsigned long prev_end = 0;
 	char line[512];
 	while (fgets(line, sizeof(line), fp)) {
-		MemRegion region;
 		char perms[5], dev[12], mapname[256] = "";
 		unsigned long offset, inode;
 
 		if (sscanf(line, "%lx-%lx %4s %lx %s %lu %255[^\n]s",
-				   &region.start, &region.end, perms, &offset, dev, &inode, mapname) >= 6) {
-			strncpy(region.perms, perms, 4);
-			region.perms[4] = '\0';
-			strncpy(region.name, mapname, 255);
-			region.name[255] = '\0';
+				   &(region->start), &(region->end), perms, &offset, dev, &inode, mapname) >= 6) {
+			strncpy(region->perms, perms, 4);
+			region->perms[4] = '\0';
+			strncpy(region->name, mapname, 255);
+			region->name[255] = '\0';
 
-			if (region_count == 0 && region.start > 0) {
-				regions[region_count].start = 0;
-				regions[region_count].end = region.start;
-				strcpy(regions[region_count].perms, "----");
-				strcpy(regions[region_count].name, "[unmapped]");
-				region_count++;
+			if (m->region_count == 0 && region->start > 0) {
+				m->regions[m->region_count].start = 0;
+				m->regions[m->region_count].end = region->start;
+				strcpy(m->regions[m->region_count].perms, "----");
+				strcpy(m->regions[m->region_count].name, "[unmapped]");
+				m->region_count++;
 			}
 
-			if (prev_end != 0 && region.start > prev_end) {
-				regions[region_count].start = prev_end;
-				regions[region_count].end = region.start;
-				strcpy(regions[region_count].perms, "----");
-				strcpy(regions[region_count].name, "[unmapped]");
-				region_count++;
+			if (prev_end != 0 && region->start > prev_end) {
+				m->regions[m->region_count].start = prev_end;
+				m->regions[m->region_count].end = region->start;
+				strcpy(m->regions[m->region_count].perms, "----");
+				strcpy(m->regions[m->region_count].name, "[unmapped]");
+				m->region_count++;
 			}
 
-			regions[region_count++] = region;
-			prev_end = region.end;
+			memcpy(&(m->regions[m->region_count++]), region, sizeof(MemRegion));
+			prev_end = region->end;
 		}
+	memset(m->regions[m->region_count-1].status, 0, sizeof(m->regions[m->region_count-1].status));
+	memset(m->regions[m->region_count-1].pid, 0, sizeof(m->regions[m->region_count-1].pid));
+	printf("%lx-%lx %s %s [%d,%d,%d] [%d,%d,%d]\n", 
+			m->regions[m->region_count-1].start, 
+			m->regions[m->region_count-1].end, 
+			m->regions[m->region_count-1].perms, 
+			m->regions[m->region_count-1].name, 
+			m->regions[m->region_count-1].pid[0], 
+			m->regions[m->region_count-1].pid[1], 
+			m->regions[m->region_count-1].pid[2], 
+			m->regions[m->region_count-1].status[0], 
+			m->regions[m->region_count-1].status[1], 
+			m->regions[m->region_count-1].status[2]);
+
 	}
 	fclose(fp);
+	free(region);
 
 	if (prev_end < ADDRESS_SPACE_END) {
-		regions[region_count].start = prev_end;
-		regions[region_count].end = ADDRESS_SPACE_END;
-		strcpy(regions[region_count].perms, "----");
-		strcpy(regions[region_count].name, "[unmapped]");
-		region_count++;
+		m->regions[m->region_count].start = prev_end;
+		m->regions[m->region_count].end = ADDRESS_SPACE_END;
+		strcpy(m->regions[m->region_count].perms, "----");
+		strcpy(m->regions[m->region_count].name, "[unmapped]");
+		m->region_count++;
 	}
 }
-void print_memory_map() {
+void print_memory_map(MemMap *m, MemMap *m2) {
 	printf("Memory Layout:\n");
 	printf("+-----------------------------------------+------+------------------------------------------+----------------------+----------------------+----------------------+\n");
 	printf("| Memory Region                           | Perms| content                                  | Result Bottom        | Result middle        | Result upper         |\n");
-	for (int i = 0; i < region_count; i++) {
+	for (int i = 0; i < m->region_count; i++) {
 
 		printf("+-----------------------------------------+------+------------------------------------------+----------------------+----------------------+----------------------+\n");
-		printf("| 0x%016lx - 0x%016lx | %s | %-40s | %-29s | %-29s | %-29s |\n",
-			   regions[i].start, regions[i].end, regions[i].perms, regions[i].name[0] ? regions[i].name : "[anonymous]",
-			   reasons[regions[i].status[0]], reasons[regions[i].status[1]], reasons[regions[i].status[2]] );
+		printf( ((m->regions[i].start == m2->regions[i].start) && (m->regions[i].end == m2->regions[i].end)) ? LINE_FORMAT_REGULAR : LINE_FORMAT_MODIFIED,
+//		printf( LINE_FORMAT_REGULAR,
+			   m->regions[i].start, m->regions[i].end, m->regions[i].perms, m->regions[i].name[0] ? m->regions[i].name : "[anonymous]",
+			   reasons[m->regions[i].status[0]], reasons[m->regions[i].status[1]], reasons[m->regions[i].status[2]] );
+//			   reasons[0], reasons[0], reasons[0] );
 	}
 	printf("+-----------------------------------------+------+------------------------------------------+----------------------+----------------------+----------------------+\n");
 }
 
-void handle_child_exit(pid_t pid, int status) {
+void handle_child_exit(MemMap *m, pid_t pid, int status) {
 	int i,j;
 
 	for (j = 0; j < 3; j++) {
-		for (i = 0; i < region_count; i++) {
-			if (regions[i].pid[j] == pid) {
+		for (i = 0; i < m->region_count; i++) {
+			if (m->regions[i].pid[j] == pid) {
 				if (WIFEXITED(status)) {
-					regions[i].status[j] = WRITEOK;
+					m->regions[i].status[j] = WRITEOK;
 				}
 				if (WIFSIGNALED(status)) {
-					regions[i].status[j] = WRITEKO;
+					m->regions[i].status[j] = WRITEKO;
 				}
 				return;
 			}
@@ -118,52 +143,74 @@ void handle_child_exit(pid_t pid, int status) {
 	printf("[handle_child_exit]unknown pid (%d) exit\n", pid);
 }
 
-void child_process(MemRegion region, int test_type) {
+void child_process(MemRegion *region, int test_type) {
 	pid_t pid = getpid();
 	int *ptr;
 	int old;
 	int i;
 
 	prctl(PR_SET_DUMPABLE, 0);
-	unsigned long addresses[3] = {region.start, (region.start + region.end) / 2, region.end - sizeof(int)};
+
+	unsigned long addresses[3] = {region->start, (region->start + region->end) / 2, region->end - 2 * sizeof(int)};
 	ptr = (int *)addresses[test_type];
+	printf("[%d] read %p region=%s test_type=%d\n", pid, ptr, region->name, test_type);
 	old = *ptr; // save old
+	printf("[%d] read %p -> %d\n", pid, ptr, old);
+
 	*ptr = ANSWER_TO_THE_ULTIMATE_QUESTION;
+	printf("[%d] read %p <- %d\n", pid, ptr, ANSWER_TO_THE_ULTIMATE_QUESTION);
+
 	if (*ptr != ANSWER_TO_THE_ULTIMATE_QUESTION) {
 		printf("the write at %p was not successful (%d)\n", ptr, *ptr);
 		exit(2);
 	}
+	printf("[%d] read %p <- %d\n", pid, ptr, old);
 	*ptr = old;
 	exit(0);
 }
 
 int main(int argc, char *argv[]) {
-	memset(regions,0, sizeof(regions));
-	parse_proc_maps();
+	void *rsp;
+
+	MemMap *m, *m2;
+
+	m = malloc(sizeof(MemMap));
+	m2 = malloc(sizeof(MemMap));
+	getchar();
+	memset(m, 0, sizeof(m));
+	memset(m2, 0, sizeof(m2));
+	__asm__("mov %%rsp, %0" : "=r"(rsp));printf("Stack Pointer (RSP): %p\n", rsp);
+ 	parse_proc_maps(m);
+	getchar();
+//        print_memory_map(&m, &m2);
 	for (int j = 0; j<3; j++){
-		for (int i = 0; i < region_count; i++) {
+		for (int i = 0; i < m->region_count; i++) {
 			pid_t pid = fork();
 			if (pid < 0) {
 				perror("Fork failed");
 				exit(EXIT_FAILURE);
 			} else if (pid == 0) {
-				child_process(regions[i], j);
+				child_process(&(m->regions[i]), j);
 			} else {
-				regions[i].pid[j] = pid;
+				m->regions[i].pid[j] = pid;
 			}
 		}
 	}
 
 	for (int j = 0; j<3; j++){
-		for (int i = 0; i < region_count; i++) {
+		for (int i = 0; i < m->region_count; i++) {
 			int status;
 			pid_t pid = wait(&status);
-			handle_child_exit(pid, status);
+			handle_child_exit(m, pid, status);
 		}
 	}
 
-	print_memory_map();
+	parse_proc_maps(m2);
+	print_memory_map(m, m2);
+	free(m);
+	free(m2);
 
+	sleep(300);
 	return 0;
 }
 
